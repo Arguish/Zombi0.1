4e736b6da2ce271b91c3f0cf53dddbc17cac11e2
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-02-06 20:51:53 +0000

    cell comentado

--- Data/src/cell.js
+++ Data/src/cell.js
@@ -2,6 +2,7 @@
 //GESTION DE CELDA
 //////
 function Cell(x, y, ref) {
+  /* este es el objeto celda, en el se almacenann las referenncias al html, gracias a este objeto y sus metodos podemos enlazar el aspecto del css conn lo que hay en los datos del js que esta funncionando de fondo */
   this.x = x;
   this.y = y;
   this.ref = ref;

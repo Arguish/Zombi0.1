548e34af3c0bf2086d73d8f59599d01599cb336b
Javier Hernandez Gonzalez <86983739+Arguish@users.noreply.github.com>
2023-02-06 19:14:12 +0000

    Solo Codigo util

--- Data/src/index.js
+++ Data/src/index.js
@@ -13,6 +13,7 @@ function gameStart() {
   timerId = setInterval((e) => {
     gameMap.updateMap();
   }, 333);
+
   window.addEventListener("keydown", (e) => {
     console.log(e.key);
     if ((e.key === "w") | (e.key === "a") | (e.key === "s") | (e.key === "d")) {
